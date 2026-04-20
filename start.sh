f48219a391be77fc422ecd8c2a76a373e7606a06
Ace <146670680+Sarah-mir@users.noreply.github.com>
2026-04-20 17:34:03 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/home/samir/.jdks/openjdk-24.0.1
-
-# Tomcat starten
-~/tomcat/bin/startup.sh
-
-# Vue starten
-cd ~/IdeaProjects/Clie-Serv-Projekt/frontend
-npm run dev
